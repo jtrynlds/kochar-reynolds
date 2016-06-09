313e11089c66726b3af2f208b94c1219457b2ad5
jtrynlds <jtrynlds99@gmail.com>
2016-06-09 13:33:05 -0400

    Update Map.java

--- Map.java
+++ Map.java
@@ -33,7 +33,7 @@ public class Map {
       }
     int terrains[] = new int[numTerritories];
     for(int i = 0; i < numTerritories; i++){
-      terrains[i] = mapScanner.nextLine();
+      terrains[i] = mapScanner.nextInt();
       sTroops[i] = mapScanner.nextInt();
       names[i] = mapScanner.nextInt();
     }
@@ -45,22 +45,51 @@ public class Map {
     }
     for(int y = 0; y < height; y ++) for(int x = 0; x < width; x ++){
       areDiagonal[map[y][x]][map[(y+1)%height][(x+1)%width]] = true;
-      areDiagonal[map[y][x]][map[(y-1)%height][(x-1)%width]] = true;
-      areDiagonal[map[y][x]][map[(y-1)%height][(x+1)%width]] = true;
-      areDiagonal[map[y][x]][map[(y+1)%height][(x-1)%width]] = true;
+      areDiagonal[map[y][x]][map[(y-1+height)%height][(x-1+width)%width]] = true;
+      areDiagonal[map[y][x]][map[(y-1+height)%height][(x+1)%width]] = true;
+      areDiagonal[map[y][x]][map[(y+1)%height][(x-1+width)%width]] = true;
     }
     for(int y = 0; y < height; y ++) for(int x = 0; x < width; x ++){
       areAdjacent[map[y][x]][map[(y+1)%height][x]] = true;
       areDiagonal[map[y][x]][map[(y+1)%height][x]] = false;
-      areAdjacent[map[y][x]][map[(y-1)%height][x]] = true;
-      areDiagonal[map[y][x]][map[(y-1)%height][x]] = false;
+      areAdjacent[map[y][x]][map[(y-1+height)%height][x]] = true;
+      areDiagonal[map[y][x]][map[(y-1+height)%height][x]] = false;
       areAdjacent[map[y][x]][map[y][(x+1)%width]] = true;
       areDiagonal[map[y][x]][map[y][(x+1)%width]] = false;
-      areAdjacent[map[y][x]][map[y][(x-1)%width]] = true;
-      areDiagonal[map[y][x]][map[y][(x-1)%width]] = false;
+      areAdjacent[map[y][x]][map[y][(x-1+width)%width]] = true;
+      areDiagonal[map[y][x]][map[y][(x-1+width)%width]] = false;
     }
-    int n = 0; //use areAdjacent and areDiagonal to generate Integer arrays adjacent[][] and diagonal[][], 
-    for(int i = 0; i < numTerritories; i++) terrs[i] = new Territory(names[i], sTroops[i], terrain[i], adjacent[i], diagonal[i]);
+    int n = 0;
+    int adjacent[][];
+    int diagonal[][];
+    while(n < numTerritories){
+      int na = 0;
+      int nd = 0;
+      for(int k = 0; k < numTerritories; k++){
+        if(areAdjacent[n][k]) na ++;
+        if(areDiagonal[n][k]) nd ++;
+      }
+      adjacent[n] = new int[na];
+      diagonal[n] = new int[nd];
+      for(int k = 0; k < numTerritories; k++){
+        if(areAdjacent[n][k]){
+          na ++;
+          adjacent[n][adjacent[n].length - na] = k;
+        }
+        if(areDiagonal[n][k]){
+          nd ++;
+          diagonal[n][diagonal[n].length - nd] = k;
+        }
+      }
+      n ++;
+    }
+    for(int i = 0; i < numTerritories; i++){
+      terrs[i] = new Territory(names[i], sTroops[i], terrain[i], adjacent[i], diagonal[i]);
+    }
   }
   public int getTile(int row, int col) return map[row][col];
+  public int getAdjacentTerrs[](int terr) return adjacent[terr];
+  public int getDiagonalTerrs[](int terr) return diagonal[terr];
+  public int getTerrSize(int terr) return terr[terr].getSize();
+  public int getTerrType(int terr) return terr[terr].getType();
 }
