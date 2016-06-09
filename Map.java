f2ec7e313bbcaf9074eb89a069b684d6b0feb75a
jtrynlds <jtrynlds99@gmail.com>
2016-06-09 13:46:20 -0400

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,33 +1,37 @@
+package net.mrpaul.ads.MM175.ps04;
 import java.io.*;
+import java.util.Scanner;
 public class Map {
   String name;
   int numTiles;
   int height;
   int width;
-  int numWaterTiles;
-  int numDesertTiles;
-  int numRegularTiles;
+  int numWaterTerritories;
+  int numDesertTerritories;
+  int numRegularTerritories;
+  int numTerritories;
   int map[][];
   Territory terrs[];
-  String names[][]= new String[height][width];
-  public Map(){
+  String names[];
+  public Map() throws FileNotFoundException{
     name = "defaultmap.csv";
     mapGen();
   }
-  public Map(String mapName){
+  public Map(String mapName) throws FileNotFoundException{
     name = mapName;
     mapGen();
   }
-  private void mapGen(){
+  private void mapGen() throws FileNotFoundException{
     Scanner mapScanner = new Scanner(new File(name));
     numTerritories = mapScanner.nextInt();
+    names = new String[numTerritories];
     height = mapScanner.nextInt();
     width = mapScanner.nextInt();
     numWaterTerritories = mapScanner.nextInt();
     numDesertTerritories = mapScanner.nextInt();
     numRegularTerritories = mapScanner.nextInt();
     map = new int[height][width];
-    int sTroops[][] = new int[height][width];
+    int sTroops[] = new int[numTerritories];
     for(int y = 0; y < height; y ++) for(int x = 0; x < height; x ++){
       map[y][x] = mapScanner.nextInt();
       }
@@ -35,7 +39,7 @@ public class Map {
     for(int i = 0; i < numTerritories; i++){
       terrains[i] = mapScanner.nextInt();
       sTroops[i] = mapScanner.nextInt();
-      names[i] = mapScanner.nextInt();
+      names[i] = mapScanner.nextLine();
     }
     boolean areAdjacent[][] = new boolean[numTiles][numTiles];
     boolean areDiagonal[][] = new boolean[numTiles][numTiles];
@@ -60,8 +64,8 @@ public class Map {
       areDiagonal[map[y][x]][map[y][(x-1+width)%width]] = false;
     }
     int n = 0;
-    int adjacent[][];
-    int diagonal[][];
+    int adjacent[][] = new int[numTerritories][];
+    int diagonal[][] = new int[numTerritories][];
     while(n < numTerritories){
       int na = 0;
       int nd = 0;
@@ -84,12 +88,23 @@ public class Map {
       n ++;
     }
     for(int i = 0; i < numTerritories; i++){
-      terrs[i] = new Territory(names[i], sTroops[i], terrain[i], adjacent[i], diagonal[i]);
+      terrs[i] = new Territory(names[i], sTroops[i], terrains[i], adjacent[i], diagonal[i]);
     }
+    mapScanner.close();
   }
-  public int getTile(int row, int col) return map[row][col];
-  public int getAdjacentTerrs[](int terr) return adjacent[terr];
-  public int getDiagonalTerrs[](int terr) return diagonal[terr];
-  public int getTerrSize(int terr) return terr[terr].getSize();
-  public int getTerrType(int terr) return terr[terr].getType();
+  public int getTile(int row, int col){
+	  return map[row][col];
+  }
+  public int[] getAdjacentTerrs(int terr){ 
+	  return terrs[terr].getAdjacent();
+  }
+  public int[] getDiagonalTerrs(int terr){
+	  return terrs[terr].getDiagonal();
+  }
+  public int getTerrSize(int terr){
+	  return terrs[terr].getSize();
+  }
+  public int getTerrType(int terr){
+	  return terrs[terr].getType();
+  }
 }
