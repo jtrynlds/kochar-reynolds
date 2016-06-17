3b2c2cacdf11611cdaf5b66ac79bcf008631327d
jtrynlds <jtrynlds99@gmail.com>
2016-06-17 10:40:12 -0400

    Update Uncertainty.java

--- Uncertainty.java
+++ Uncertainty.java
@@ -1,12 +1,19 @@
 import java.util.*;
 public class Uncertainty {
   public static void main(String[] args){
-    String messages[] = {"", "", ""}
+    String messages[] = {"", "", "", "", "", ""}
     Scanner scanner = new Scanner(System.in);
     System.out.print(messages[0]);
     int numPlayers = scanner.nextInt();
     System.out.print(messages[1]);
     String mapFileName = scanner.nextLine();
     System.out.print(messages[2]);
+    int numPlayers = scanner.nextInt();
+    System.out.print(messages[3]);
+    int playerBudget = scanner.nextInt();
+    System.out.print(messages[4]);
+    Map map;
+    if(mapFileName == "") map = new Map();
+    else map = new Map(mapFileName);
   }
 }
